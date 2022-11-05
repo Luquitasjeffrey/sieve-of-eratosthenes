aa52c5d2d4db05c11e78d4f582982ce0ceb7cc1c
Luquitasjeffrey <105951354+Luquitasjeffrey@users.noreply.github.com>
2022-11-05 17:32:00 -0300

    Update erathostenes.cpp

--- erathostenes.cpp
+++ erathostenes.cpp
@@ -3,46 +3,8 @@
 #include <cstdlib>
 #include <chrono>
 #include <cstring>
-#include <cmath>
 #include <vector>
 using namespace std;
-
-void createsieve(uint64_t * arr, uint32_t arrlen){//this is not the sieve of eratosthenes
-    arr[0]=3;
-    uint64_t start=5;
-    uint32_t arrpos=1;
-    for(; arrpos<arrlen; arrpos++){
-        uint32_t i=0;
-        while(arr[i]*arr[i]<=start){
-            if(start%arr[i]==0){
-                i=0;
-                start+=2;
-                continue;
-            }
-            i++;
-        }
-        arr[arrpos]=start;
-        start+=2;
-    }
-}
-
-void trivialp(uint64_t * arr, uint32_t arrlen){
-    arr[0]=3;
-    uint64_t start=5;
-    for(int pos=1; pos<arrlen; pos++){
-        uint32_t i=3;
-        while(i*i<=start){
-            if(start%i==0){
-                start+=2;
-                i=3;
-                continue;
-            }
-            i+=2;
-        }
-        arr[pos]=start;
-        start+=2;
-    }
-}
 
 struct bitarray{
     uint8_t * arr;
@@ -78,7 +40,7 @@ struct bitarray{
     }
 };
 
-void print_bitarr(bitarray &prt){
+void print_bitarr(bitarray &prt){//call this function if you want to see the sieve as numbers of 0 or 1
     int i=0;
     for(; i<prt.arrsize; i++){
         printf("%i", (int)prt[i]);
@@ -121,13 +83,15 @@ void eratosthenes(uint64_t until, vector<uint64_t> &out){
 int main(){
     vector<uint64_t> erat;
     uint64_t stop;
+    printf("enter in wich number you want the algorithm to stop");
     scanf("%lu", &stop);
     clock_t t=clock();
     eratosthenes(stop, erat);
     printf("the time of erath is: %f\n\n", (double)(clock()-t)/((double)CLOCKS_PER_SEC));
     printf("last prime: %lu\n", erat[erat.size()-1]);
     printf("vector size is: %lu\n", erat.size());
-
+    
+    //quit the comments if you want to see all the numbers
     /*
     uint32_t i=0;
     for(; i<erat.size(); i++){
@@ -136,27 +100,3 @@ int main(){
     */
     return 0;
 }
-
-#if 0
-int main(){
-
-    uint32_t len;
-    cin>>len;
-    uint64_t * arr=(uint64_t*)malloc(len*sizeof(uint64_t));
-    clock_t start;
-
-    start=clock();
-    createsieve(arr, len);
-    printf("timer of my sieve is: %f\n\n", (double)(clock()-start)/((double)CLOCKS_PER_SEC));
-
-    vector<uint64_t> justforfun;
-    start=clock();
-    eratosthenes(arr[len-1], justforfun);
-    printf("timer of erath is: %f\n\n", (double)(clock()-start)/((double)CLOCKS_PER_SEC));
-
-    start=clock();
-    trivialp(arr, len);
-    printf("timer of trivial div is: %f\n\n", (double)(clock()-start)/((double)CLOCKS_PER_SEC));
-    return 0;
-}
-#endif
